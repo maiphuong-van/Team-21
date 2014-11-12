cfb745e45dbd8ae4cdcbe228de6a3f26af888231
Mai Phuong Van <maiphuong.van93@gmail.com>
2014-11-12 17:33:52 +0100

    Change name

--- Simple BJ/game21.erl
+++ Simple BJ/game21.erl
@@ -1,16 +1,16 @@
--module (game21_client_server).
+-module (game21).
 -import (game21_deck, [deck/0, shuffled/1]).
 
 %-define(SUITS, [diamond, heart, club, spade] ).
 %-define(CARDS, [{ace,1},{2,2},{3,3},{4,4},{5,5},{6,6},{7,7},{8,8},{9,9},{10,10},{jack,10},{queen,10},{king,10}]).
--export([start/0,stop/0, init/0,add_deck/1,usr_bet/1,
+-export([start/0,stop/0, init/0,usr_bet/1,
   usr_double_down/0,usr_hit/0,usr_stand/0,make_deck/0,usr_start/0]).
 
 start() ->
   Bj_Pid = whereis(bjgame),
   case Bj_Pid of 
     undefined ->   
-      register (bjgame, Pid = spawn(game21_client_server,init,[])),
+      register (bjgame, Pid = spawn(game21,init,[])),
       {ok, Pid};
     _         -> 
       {ok, Bj_Pid}      
@@ -40,21 +40,29 @@ member(Pid,L)  when length (L) >=1  ->
 loop(List) -> 
   receive 
   {From, start} -> 
-    From! {started, 'Please make a bet'},
-    loop ([ {From,make_deck(),0,0} |List]);
-  {From, add_deck, N} -> 
-    Num = N,
     case member(From, List) of 
-      undefined -> 
-        From! {not_added, 'You have to start first'},
-        loop (List);
+      undefined ->
+        From! {started, 'Please make a bet'},
+        loop ([{From,make_deck(),0,0} |List]);
       _ -> 
+        From! {started, 'Please make a bet'},
         Deck = element (1,member(From, List)),
-        Add = lists:append(lists:duplicate(Num, make_deck())),
-        New_deck = Add ++ Deck,
-        From! {added, Num},
-        loop ([ {Pid, New_deck, B, P} || {Pid, _, B, P} <- List])
-    end;
+        Add = make_deck(),
+        New_deck = Deck + Add,
+        loop([{From,New_deck,0,0}] ++ [{Pid, New_deck, B, P} || {Pid, _, B, P} <- List]);
+%  {From, add_deck, N} -> 
+%    Num = N,
+%    case member(From, List) of 
+%      undefined -> 
+%        From! {not_added, 'You have to start first'},
+%        loop (List);
+%      _ -> 
+%        Deck = element (1,member(From, List)),
+%        Add = lists:append(lists:duplicate(Num, make_deck())),
+%        New_deck = Add ++ Deck,
+%        From! {added, Num},
+%        loop ([ {Pid, New_deck, B, P} || {Pid, _, B, P} <- List])
+%    end;
   {From, bet, N} ->
     Num = N,
     case member(From, List) of 
@@ -69,14 +77,14 @@ loop(List) ->
     end
   end.
 
-add_deck(N) -> 
-  bjgame! {self(), add_deck, N},
-  receive 
-    {added, Msg}     -> io:format('Added ~p decks to current deck ~n', [Msg]);
-    {not_added, Msg} -> Msg
-  after 1000 ->
-    timeout  
-  end. 
+%add_deck(N) -> 
+%  bjgame! {self(), add_deck, N},
+%  receive 
+%    {added, Msg}     -> io:format('Added ~p decks to current deck ~n', [Msg]);
+%    {not_added, Msg} -> Msg
+%  after 1000 ->
+%    timeout  
+%  end. 
 
 usr_start() -> 
   bjgame! {self(), start},
@@ -102,6 +110,3 @@ usr_double_down()-> ok.
 usr_hit () -> ok.
 
 usr_stand() -> ok.
-
-%take out a card
-deal() -> ok.
