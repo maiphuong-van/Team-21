797e21bf74a1f2f8ccb117158d7c9d9efbf88af3
Mai Phuong Van <maiphuong.van93@gmail.com>
2014-12-01 01:48:48 +0100

    Game logic
    Comments with logic of the game, so it will be easier to follow the flow
    while implementing

--- Simple BJ/game21.erl
+++ Simple BJ/game21.erl
@@ -32,7 +32,7 @@ init() -> loop ([]).
 
 member(_Pid,L) when L == []-> undefined;
 member(Pid,L)   -> 
-  if Pid == element(1, hd (L)) -> {element(2, hd (L)),element(3, hd (L)),element(4, hd (L)), element(5, hd (L)), element(6, hd (L))};
+  if Pid == element(1, hd (L)) -> hd(L);
       true                     -> member(Pid,tl(L))
   end.
 
@@ -42,7 +42,7 @@ loop(List) ->
   {From, start} ->
     From! {started, 'Please make a bet'},
     %what to save here: Pid, deck, bet, user point, dealer point, user card, dealer card.
-    loop ([{From,make_deck(),0,0,0, [], []} |List]);
+    loop ([{From,make_deck(),0,0,0, [], [], 1000} |List]);
 
   {From, bet, N} ->
     case member(From, List) of 
@@ -54,22 +54,46 @@ loop(List) ->
         % after bet, deals 2 cards, let the player knows their point
         % also dealer deals 2 cards and save the points
         From! {bet, N},
-        loop ([{Pid,D, N, UsrP, DlrP, UsrC, DlrC} || {Pid, D, _, UsrP, DlrP, UsrC, DlrC} <- List, Pid == From])
+        loop ([{Pid,D, N, UsrP, DlrP, UsrC, DlrC, M} || {Pid, D, _, UsrP, DlrP, UsrC, DlrC, M} <- List, Pid == From])
     end
-
+  %{From, hit}->
+  %Check if user has started the game
+  %No -> error message
+  %Yes -> Deal one more care to user
+  %Check the point
+  %If under 21
+  %(Not sure if dealer deal 1 more card or not
+    %probably yes, deal 1 card to dealer and check point, if over 21, send win message
+    %else send hit or stand)
+  %send message hit or stand
+  % if over 21, send loosing message 
+ 
+  %{From, stand}->
+  %Check if user has started the game
+  %No then send error message
+  %Yes, check dealer's point, if under 16 then add deal more cards to dealer until it's over 16
+  %check if DlrP is over 21, if yes send win message
+  %else, then compare dealer and user's point
+  %Send win or loose message
 
   %{From, double}->
-    %case member(From, List) of 
+  %Check if the user has initial bet
+    %case  of 
       %undefined -> 
-      %From! {not_bet, 'You have to start first'},
+      %From! {not_bet, 'You have to bet first'},
      % loop (List);
     %_ -> 
+      % Get the initial bet, double it
       %double_bet= element(2,menber(From, List))*2,
+      %Get one more new card
       %new_card = deal(element(1,menber(From, List))),
+      %Get the points user has before, add with new card's point
       %user_newpoint= element(4,menber(From,List)+element(2, 
-         
+      %(check if user point over 21, if yes then user lose) 
+      % else get dealer's point
+      % add card and calculate new point until dealer's point is bigger than 16   
       %dealer_point=
-
+      % check who has higher point and below 21
       %if  
         %check funtion with if its over 21
         %compare user with dealer points, user win then send win messgage
@@ -77,9 +101,12 @@ loop(List) ->
         %true
         %  From! {double, lose}
       %end
+      %calculate how much money is left
       %user_moneyleft=
+      % new deck is the deck before -- all cards dealt 
       % new_deck=  cards left in deck 
 
+      %loop initial bet, new deck, no point for user, dealer, no card dealt, new amount of money
       %loop([{Pid, D, N, 0, 0, [],[]}|| {Pid, _, N, _,_,_,_} <- List, Pid == From, D==new_deck])  
     
   %end.
@@ -107,7 +134,7 @@ usr_bet(N) ->
 
 
 
-% user chosen double down or hit or stay. once chosen hit, double down won't show in next trun, (only showing hit or stand)
+% user chosen double down or hit or stay. once chosen hit, double down won't show in next turn, (only showing hit or stand)
 % when chosen double, user will recive one more card and double the bet and game end.
 %usr_double_down()-> 
 %     bjgame! {self(), double},
@@ -121,3 +148,11 @@ usr_bet(N) ->
 usr_hit () -> ok.
 
 usr_stand() -> ok.
+
+
+point(C, UsrP) -> ok.
+%point is to check card's point
+%If card is Ace, check UsrP, UsrP is less than 10, return 11+ UsrP
+%If UsrP over 10, return 1+ UsrP
+%Else, get card's point in the tuple and then plus with UsrP
+
